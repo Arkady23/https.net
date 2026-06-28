0197960a3354d2bf6d0a962394d7443549c3e596
Arkady <kornienko@outlook.com>
2026-06-28 15:52:48 +1000

    Update initcgi.py

--- www/initcgi.py
+++ www/initcgi.py
@@ -1,7 +1,7 @@
-﻿#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #!!  Взаимообмен между http.net/https.net сервером и скриптами  !!
 #!!  по протоколу initCGI                                       !!
-#!!  Автор: А.Корниенко                             27.05.2026  !!
+#!!  Автор: А.Корниенко & AI Collaborator           21.06.2026  !!
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 #    Протокол быстрого CGI: initCGI (C)
@@ -16,22 +16,30 @@
 # 4. Весь остальной поток POST в стандартном вводе проходит
 #    скрипту-обработчику без изменений.
 
-import os, sys, runpy
+import os, sys, importlib.util          # в другом случае  runpy
 
 n = int("0"+sys.stdin.readline())       # Чтение длины заголовка
 if n:
 
    # Чтение блока заголовков и формирование переменных окружения:
-   z = sys.stdin.read(n)+"\n"
-   i = z.find("\n")
-   script = z[0:i]
-   i += 1
-   while i < n:
-      k = z.find(":", i)
-      name = z[i:k]
-      k += 1
-      i = z.find("\n", k)
-      os.environ[name] = z[k:i]
-      i += 1
+   lines = sys.stdin.read(n).split('\n')
 
-   runpy.run_path(script)    # Запуск скрипта обработчика запроса
+   script = lines[0]
+
+   # Безопасная разбирка заголовков без риска уронить скрипт
+   for line in lines[1:]:
+       name, sep, value = line.partition(':')
+       if sep:
+          os.environ[name.strip()] = value.strip(' \r')
+
+   # Получение и установка пути:
+   os.chdir(os.path.dirname(os.path.abspath(script)))
+
+   #runpy.run_path(script)   # Запуск скрипта обработчика запроса
+   #Более быстрый вариант от AI Collaborator:
+   spec = importlib.util.spec_from_file_location("cgi_script", script)
+   module = importlib.util.module_from_spec(spec)
+   spec.loader.exec_module(module)
+
+   # Очистка памяти для отладки при изменении скриптов
+   sys.modules.pop("cgi_script", None)
